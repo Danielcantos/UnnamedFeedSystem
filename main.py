8d445b400151c7abf55f3d3560df090442cede56
Danielcantos <danielcantosgalvez@gmail.com>
2024-01-29 21:13:07 +0100

    Main file definition

---
+++ main.py
@@ -0,0 +1,7 @@
+# SCUBE-AEther feed system calculations prorgram
+# Developed by the Feed System Team 2023-24:
+# - Daniel Cantos Gálvez (danielcantosgalvez@gmail.com)
+# - Hemanth Alapati
+# - Louis Urbin
+# - Stanislas Le Person
+# - Thomas Roberge
